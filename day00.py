ecf1041797d3f3c565b4e63382a17be52775574c
codybartfast <john@codybartfast.com>
2026-01-15 15:11:57 +0000

    Day01: pre - more bells

--- day00.py
+++ day00.py
@@ -1,24 +1,18 @@
 def parse(input_text):
     return input_text
 
-def part1(input):
-    ...
-
-def part2(input):
-    ...
-
 def bells(input_text):
     input = parse(input_text)
-    print(input)
-    ans1 = part1(input)
-    ans2 = part2(input)
-    print(f"Part1: {ans1}")
-    print(f"Part2: {ans2}")
+    print(input, "\n")
+    yield input
+    yield None
+    yield None
 
 def jingle(input_filename = None):
     import sack
-    input_text = sack.get_input(input_filename)
-    bells(input_text)
-
+    input_text = sack.read_input(input_filename)
+    sack.present(lambda: bells(input_text))
+    
+    
 if __name__ == "__main__":
     jingle("test1")
--- sack.py
+++ sack.py
@@ -1,13 +1,59 @@
 from pathlib import Path
 import sys
 
-def get_input(input_filename = None):
+def _day():
+    filepath = sys._getframe(2).f_code.co_filename
+    day = filepath[-5:-3]
+    return day
+ 
+def read_input(input_filename = None):
     if input_filename == None:
         input_filename = "input"    
-    filepath = sys._getframe(1).f_code.co_filename
-    day = filepath[-5:-3]
+    day = _day()
     input_path = Path(__file__).resolve().parent / "input" / "2025"
     input_path = input_path / f"day{day}" / f"{input_filename}.txt"
     with open(input_path, encoding="utf-8") as f:
          input = f.read()
     return input 
+
+
+def present(solver_action, title=None):
+    import time
+    pc_start = time.perf_counter()
+    day = _day()
+
+    if title == None:
+        title = f"Day {day}"
+
+    print()
+    print(title)
+    print("=" * len(title))
+    print()
+
+    solve_it=solver_action()
+    
+    pc_parse_before = time.perf_counter()
+    data = next(solve_it)
+    pc_parse_after = time.perf_counter()
+    
+    pc_part1_before = time.perf_counter()
+    ans1 = next(solve_it)
+    pc_part1_after = time.perf_counter()
+    print(f"Part 1: {ans1}")
+
+    pc_part2_before = time.perf_counter()
+    ans2 = next(solve_it)
+    pc_part2_after = time.perf_counter()
+    print(f"Part 2: {ans2}")
+
+    pc_stop = time.perf_counter()
+
+    print()
+    print("Timings")
+    print("---------------------")
+    print(f"  Parse: {pc_parse_after - pc_parse_before:12.6f}")
+    print(f" Part 1: {pc_part1_after - pc_part1_before:12.6f}")
+    print(f" Part 2: {pc_part2_after - pc_part2_before:12.6f}")
+    print(f"Elapsed: {pc_stop - pc_start:12.6f}")
+    print()
+     
