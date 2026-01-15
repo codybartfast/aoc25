2ea4329ed612abb04827cc6224162a1bad9537ad
codybartfast <john@codybartfast.com>
2026-01-15 20:12:53 +0000

    Day02: tidy

--- day01.py
+++ day01.py
@@ -1,39 +1,45 @@
 def parse(input_text):
     return [(line[0], int(line[1:])) for line in input_text.splitlines()]
 
+
 def turn(start, instr):
     dir, turns = instr
     if turns % 100 == 0:
         raise NotImplementedError("This case probably not covered")
-    stop = (start - turns if dir == 'L' else start + turns) % 100
+    stop = (start - turns if dir == "L" else start + turns) % 100
 
     zero_clicks = turns // 100
-    if (start != 0 and stop != 0 and
-            ((dir == "L" and start < stop)
-                or (dir == "R" and stop < start))):
+    if (
+        start != 0
+        and stop != 0
+        and ((dir == "L" and start < stop) or (dir == "R" and stop < start))
+    ):
         zero_clicks += 1
     return (stop, zero_clicks)
-    
+
+
 def bells(input_text):
     instructions = parse(input_text)
     yield instructions
 
-    position = 50
+    pstn = 50
     zero_stops = 0
     zero_clicks = 0
     for instr in instructions:
-        position, clicks = turn(position, instr)
-        zero_stops += position == 0
+        pstn, clicks = turn(pstn, instr)
+        zero_stops += pstn == 0
         zero_clicks += clicks
 
     yield zero_stops
     yield zero_stops + zero_clicks
 
-def jingle(filename = None):
+
+def jingle(filename=None):
     import sack
+
     input_text = sack.read_input(filename)
     sack.present(lambda: bells(input_text))
-    
-               
+
+
 if __name__ == "__main__":
     jingle()
