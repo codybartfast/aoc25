72c63351c93a33cff1dfb6d153acb3e975d51f91
codybartfast <john@codybartfast.com>
2026-01-18 11:43:29 +0000

    △

--- day07.py
+++ day07.py
@@ -1,38 +1,31 @@
-def parse(input):
-    return input.splitlines()
-
-def deflect(beams, row):
+def split(beams, row):
     nhits = 0
     new_beams = beams.copy()
-    for idx, m in enumerate(row):
+    for idx, (item, nbeams) in enumerate(zip(row, beams)):
         nbeams = beams[idx]
-        if nbeams > 0 and row[idx] == "^":
+        if nbeams > 0 and item == "^":
             nhits += 1
             new_beams[idx - 1] += nbeams
             new_beams[idx] = 0
             new_beams[idx + 1] += nbeams
-             
+
     return new_beams, nhits
 
-def part1(manifold):
+
+def shine(manifold):
     beams = [0] * len(manifold[0])
     beams[manifold[0].index("S")] = 1
     nhits = 0
     for row in manifold:
-        beams, hits = deflect(beams, row)
+        beams, hits = split(beams, row)
         nhits += hits
     return nhits, sum(beams)
-            
+
+
 def bells(input):
-    manifold = parse(input)
-
+    manifold = input.splitlines()
     yield None
-
-    nhits, nbeams = part1(manifold)
-    
-    yield nhits
-
-    yield nbeams
+    yield from shine(manifold)
 
 
 def jingle(input_filename=None):
@@ -44,6 +37,7 @@ def jingle(input_filename=None):
 
 if __name__ == "__main__":
     import sys
+
     input_filename = sys.argv[1] if len(sys.argv) > 1 else None
     print(f"sack: filename: {input_filename}")
     jingle(input_filename)
